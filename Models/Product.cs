e7e21f8a70f1cdd444009ee4ba878b789623a968
JohnKamaujk <johnjkkamau@gmail.com>
2024-11-01 23:31:11 +0300

    :wrench:config: product model validation

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SimpleAPI.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string? Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
         public decimal Price { get; set; }
     }
 }
